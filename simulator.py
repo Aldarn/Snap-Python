188200a033945917e4b41e917d1531e5c9a64039
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-02-17 21:00:55 +0000

    First draft of simulator script.

--- simulator.py
+++ simulator.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python2.7
 
+import core.game as game
+import core.dealer as dealer
+import service.player_service as playerService
+
 def main():
-	print "simulator running!"
+	print "Welcome to digital Snap!\n\n"
+
+	# Request number of decks
+	numberOfDecks = raw_input("How many decks would you like to play with? ")
+
+	# Request number of players
+	numberOfPlayers = raw_input("How many players would you like to play with (max 10)? ")
+
+	# Start the simulation
+	game.start(dealer.getDeck(numberOfDecks), playerService.getNPCs(numberOfPlayers))
 
 if __name__ == '__main__':
 	main()
