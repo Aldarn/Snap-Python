8dba61d74a8e409c7963b22450eef6c8d34e7cc5
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-02-20 18:42:42 +0000

    Removed contrived launch script tests

--- tests/test_dealer.py
+++ tests/test_dealer.py
@@ -2,25 +2,25 @@
 
 import unittest
 import os
-import core.dealer as dealer
+from core.dealer import getDeck
 
-class DealerTests(unittest.TestCase):
+class TestDealer(unittest.TestCase):
 	def testDeckIsFull(self):
 		numberOfPacks = 1
-		deck = dealer.getDeck(numberOfPacks, shuffled = False)
+		deck = getDeck(numberOfPacks, shuffled = False)
 		self.failUnlessEqual(len(deck), 52)
 
 	def testMultiplePacks(self):
 		numberOfPacks = 3
-		deck = dealer.getDeck(numberOfPacks, shuffled = False)
+		deck = getDeck(numberOfPacks, shuffled = False)
 		self.failUnlessEqual(len(deck), 52 * numberOfPacks)
 
 	def testDeckIsShuffled(self):
 		# High number to prevent the chance of the shuffle returning them in the same order as they're created
 		# TODO: This should be improved using mocks and verifying the shuffle is called
 		numberOfPacks = 10
-		shuffledDeck = dealer.getDeck(numberOfPacks, shuffled = True)
-		deck = dealer.getDeck(numberOfPacks, shuffled = False)
+		shuffledDeck = getDeck(numberOfPacks, shuffled = True)
+		deck = getDeck(numberOfPacks, shuffled = False)
 		self.failIfEqual(shuffledDeck, deck)
 
 def main():
