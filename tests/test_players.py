b4c1b8168a628bfc2310b291714fc7b4ea2943eb
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-02-22 21:15:26 +0000

    Added player tests.

--- tests/test_players.py
+++ tests/test_players.py
@@ -3,12 +3,67 @@
 import unittest
 from service.player_service import getNPCs
 from domain.npc import NPC
+from domain.card import Card
 
 class TestPlayers(unittest.TestCase):
 	def testGetNpcs(self):
 		numberOfNpcs = 7
 		npcs = getNPCs(numberOfNpcs)
 		self.failUnlessEqual(numberOfNpcs, len(npcs))
+
+	def testAddCards(self):
+		cardsToAdd = [Card("a", "h"), Card("4", "s")]
+		npc = getNPCs(1)[0]
+		npc.addCards(cardsToAdd)
+		self.failUnlessEqual(cardsToAdd, npc._cards)
+
+	def testRemoveCards(self):
+		cardsToRemove = 2
+		cards = [Card("a", "h"), Card("4", "s"), Card("7", "c")]
+		npc = getNPCs(1)[0]
+		npc.addCards(cards)
+		npc.removeCards(cardsToRemove)
+		self.failUnlessEqual(cards[0:1], npc._cards)
+
+	def testRemoveTooManyCards(self):
+		cardsToRemove = 2
+		cards = [Card("a", "h")]
+		npc = getNPCs(1)[0]
+		npc.addCards(cards)
+		npc.removeCards(cardsToRemove)
+		self.failUnlessEqual([], npc._cards)
+
+	def testCountCards(self):
+		npc = getNPCs(1)[0]
+		npc.addCards([Card("a", "h"), Card("4", "s")])
+		self.failUnlessEqual(2, npc.getCardCount())
+
+	def testResetHand(self):
+		npc = getNPCs(1)[0]
+		npc.addCards([Card("a", "h"), Card("4", "s")])
+		npc.resetHand()
+		self.failUnlessEqual([], npc._cards)
+
+	def testNPCDoMoveSnap(self):
+		npc = NPC({"name": "test", "speed": 1.0, "accuracy": 1.0})
+		npc.doMove(True)
+		self.failUnless(npc.snapResponseTime > 0)
+
+	def testNPCDoMoveIsSnap(self):
+		npc = NPC({"name": "test", "speed": 1.0, "accuracy": 1.0})
+		npc.doMove(True)
+		self.failUnless(npc.snapResponseTime > 0)
+
+	def testNPCDoMoveNoSnapNoMiss(self):
+		npc = NPC({"name": "test", "speed": 1.0, "accuracy": 1.0})
+		npc.snapResponseTime = 1
+		npc.doMove(False)
+		self.failUnless(npc.snapResponseTime < 0)
+
+	def testNPCDoMoveNoSnapMiss(self):
+		npc = NPC({"name": "test", "speed": 1.0, "accuracy": 0.0})
+		npc.doMove(False)
+		self.failUnless(npc.snapResponseTime > 0)
 
 def main():
 	unittest.main()
