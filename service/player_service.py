9d00e1502430f99cd924748a6e8457d6d8e16a30
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-02-22 01:09:03 +0000

    Updated player service to pick NPCs randomly and validate the number of players chosen

--- service/player_service.py
+++ service/player_service.py
@@ -1,8 +1,29 @@
 from domain.npc import NPC
 import resources.resources as resources
-import simplejson
 
-npcs = simplejson.load(open(resources.getResourcePath("npcs.json"), 'r'))
+import simplejson
+import random
+
+NPCS = simplejson.load(open(resources.getResourcePath("npcs.json"), 'r'))
 
 def getNPCs(numberOfNPCs):
-	return [NPC(npcs.pop()) for n in range(0, numberOfNPCs)]
+	"""
+	Gets a list of a chosen number of randomly selected NPCs.
+
+	:param numberOfNPCs:
+		The number of NPCs to get.
+	:return:
+		List of random NPCs.
+	"""
+	return [NPC(npc) for npc in random.sample(NPCS, numberOfNPCs)]
+
+def isValidNumberOfPlayers(numberOfPlayers):
+	"""
+	Checks if the number of players is valid.
+
+	:param numberOfPlayers:
+		The number to check.
+	:return:
+		True if valid, false otherwise.
+	"""
+	return 1 < numberOfPlayers < 11
